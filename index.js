13c38e4be61787b0ea64d98ee482c1b2ef2077c2
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-01-05 00:40:20 +0000

    adding regex for title

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const token = core.getInput('repo-token');
+  const token = core.getInput('github-token');
+  const titleRegex = core.getInput('title-regex');
   const client = new github.GitHub(token);
   const payload = github.context.payload;
   const pullRequest = github.context.issue;
@@ -12,13 +13,23 @@ try {
   console.log("title", title);
   console.log("body", body);
 
-  client.pulls.createReview({
-    owner: pullRequest.owner,
-    repo: pullRequest.repo,
-    pull_number: pullRequest.number,
-    body: 'This is a comment!',
-    event: 'COMMENT'
-  });
+  let comment;
+  if (!new Regex(titleRegex).test(title)) {
+    comment = 'Incorrect title format, correct format is "{hr|engage|recruit|core|pay|work|perform|learn|analytics}/{feature|fix}/{description}".'
+  }
+
+  if (comment) {
+    client.pulls.createReview({
+      owner: pullRequest.owner,
+      repo: pullRequest.repo,
+      pull_number: pullRequest.number,
+      body: comment,
+      event: 'COMMENT'
+    });
+    core.setFailed('Pull Request lint failed.');
+  }
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
