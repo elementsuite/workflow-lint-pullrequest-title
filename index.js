cea317f90c09c3c603d4dfa1c5c1ccb691bdcb70
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-01-05 01:13:46 +0000

    removing body regex for now

--- index.js
+++ index.js
@@ -4,23 +4,15 @@ const github = require('@actions/github');
 try {
   const token = core.getInput('github-token');
   const titleRegex = core.getInput('title-regex');
-  const bodyRegex = core.getInput('body-regex');
   const client = new github.GitHub(token);
   const payload = github.context.payload;
   const pullRequest = github.context.issue;
   const title = payload.pull_request.title;
   const body = payload.pull_request.body;
 
-  console.log("title", title);
-  console.log("body", body);
-
   let comment;
   if (!new RegExp(titleRegex).test(title)) {
     comment = `Incorrect title format, regex for correct format is "${titleRegex}".`;
-  }
-
-  if (!new RegExp(bodyRegex).test(body)) {
-    comment = `Incorrect body format, regex for correct format is "${bodyRegex}".`;
   }
 
   if (comment) {
